a9b6ab719628d521982ae6730802f2111984c8d6
Michał Kordulewski <michal@kordulewski.pl>
2017-07-22 11:14:35 +0200

    obrazki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Product } from './types/product';
 })
 export class AppComponent {
   public items: Product[] = [
-    {name: 'Product 1', promoted: true, price: 2.34},
-    {name: 'Product 2', promoted: false, price: 11.30},
-    {name: 'Product 3', promoted: true, price: 312.99},
-    {name: 'Product 4', promoted: false, price: 30}
+    {name: 'Product 1', promoted: true, price: 2.34, description: 'Polecamy zakup', image: 'https://images-na.ssl-images-amazon.com/images/I/41K154iAmLL._SL150_.jpg'},
+    {name: 'Product 2', promoted: false, price: 11.30, description: 'Kup to', image: 'https://images-na.ssl-images-amazon.com/images/I/51KSVHO3h1L._SL150_.jpg'},
+    {name: 'Product 3', promoted: true, price: 312.99, description: 'Vin Diesel, Dwayne \'The Rock\' Johnson', image: 'https://images-na.ssl-images-amazon.com/images/I/51HowSWOpHL._SL150_.jpg'},
+    {name: 'Product 4', promoted: false, price: 30, description: 'Zajefajny produkt', image: 'https://images-na.ssl-images-amazon.com/images/I/514y53H8YqL._SL150_.jpg'}
   ]
 }
--- src/app/types/product.ts
+++ src/app/types/product.ts
@@ -2,4 +2,6 @@ export interface Product {
   name: string;
   promoted: boolean;
   price: number;
+  description: string;
+  image: string;
 }
