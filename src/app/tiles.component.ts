a8e73636dcddb9dcd28cc5a97d541f8cbf4c8d72
rb24009 <mlysenko@bluesoft.net.pl>
2017-07-22 13:08:57 +0200

    Wyszukiwanie

--- src/app/tiles.component.ts
+++ src/app/tiles.component.ts
@@ -4,11 +4,18 @@ import { Tile } from "./tile.component";
 @Component({
   selector: 'tiles',
   template: `
-    <div *ngFor="let item of tiles">
+  <div>
+      <div>
+  <input (keyup)="onKey($event)">
+  <p>{{values}}</p>
+       </div>
+
+    <div *ngFor="let item of filteredTiles">
       <div class="view">
         <my-tile [tile]="item"></my-tile>
       </div>
     </div>
+     </div>
   `,
   styles: [`:host {
     
@@ -16,4 +23,13 @@ import { Tile } from "./tile.component";
 })
 export class Tiles {
   @Input() public tiles: Tile[];
+  filteredTiles = this.tiles;
+
+  onKey(event: any) { // without type info
+    this.filteredTiles = this.tiles.filter(filterByName);
+    function filterByName(value) {
+      return  value.name.indexOf(event.target.value) !== -1
+
+    }
+  }
 }
