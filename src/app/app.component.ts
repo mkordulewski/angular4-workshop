9b19fd648524ae14174e27aec42461c8a9379b69
paolostyle <tuptus95@gmail.com>
2017-07-22 10:51:45 +0200

    css and product interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Product } from './types/product';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,10 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public items = [
-    {name: 'Product 1', promoted: true},
-    {name: 'Product 2', promoted: false},
-    {name: 'Product 3', promoted: true},
-    {name: 'Product 4', promoted: false}
+  public items: Product[] = [
+    {name: 'Product 1', promoted: true, price: 2.34},
+    {name: 'Product 2', promoted: false, price: 11.30},
+    {name: 'Product 3', promoted: true, price: 312.99},
+    {name: 'Product 4', promoted: false, price: 30}
   ]
 }
---
+++ src/app/types/product.ts
@@ -0,0 +1,5 @@
+export interface Product {
+  name: string;
+  promoted: boolean;
+  price: number;
+}
