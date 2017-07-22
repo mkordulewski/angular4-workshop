3fc6afabdfcd0cba2f1b721cef5c793b5f067113
paolostyle <tuptus95@gmail.com>
2017-07-22 10:28:47 +0200

    tasks 2, 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public items = [
+    'Product 1',
+    'Product 2',
+    'Product 3'
+  ]
 }
