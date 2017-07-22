02a979e09b1fb61ad00f45328be23bd527cd1b7b
paolostyle <tuptus95@gmail.com>
2017-07-22 11:34:33 +0200

    tile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Tile} from "./tile.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Tile
   ],
   imports: [
     BrowserModule
---
+++ src/app/tile.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from '@angular/core';
+import { Product } from './types/product';
+
+@Component({
+  selector: 'my-tile',
+  template: `
+    <div class="tile">Product: <em>{{ tile.name }} - {{ tile.price }}</em>
+      <img [src]="tile.image">
+      <p>{{ tile.description }}</p>
+    </div>
+  `,
+  styles: [`:host {
+    display: block;
+    padding: 10px;
+    background: #eee;
+    margin: 20px;
+  }`]
+})
+export class Tile {
+  @Input() public tile: Product;
+}
